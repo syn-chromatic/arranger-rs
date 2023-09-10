dbc9783da462369e43f03e4a67b850b946bc3e5c
syn-chromatic <synchromatic.github@gmail.com>
2023-09-10 03:19:14 +0300

    update

--- src/threading/thread_structs.rs
+++ src/threading/thread_structs.rs
@@ -1,58 +1,7 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc;
-use std::sync::{Mutex, RwLock};
+use std::sync::Mutex;
 use std::time::{Duration, Instant};
-
-pub struct RwUsize {
-    value: RwLock<usize>,
-}
-
-impl RwUsize {
-    pub fn new() -> Self {
-        let value: RwLock<usize> = RwLock::new(0);
-        RwUsize { value }
-    }
-
-    pub fn increment(&self) -> Result<(), &'static str> {
-        match self.value.write() {
-            Ok(mut write_guard) => {
-                *write_guard += 1;
-                Ok(())
-            }
-            Err(_) => Err("Failed to acquire write lock"),
-        }
-    }
-
-    pub fn decrement(&self) -> Result<(), &'static str> {
-        match self.value.write() {
-            Ok(mut write_guard) => {
-                if *write_guard == 0 {
-                    return Err("Value is already 0");
-                }
-                *write_guard -= 1;
-                Ok(())
-            }
-            Err(_) => Err("Failed to acquire write lock"),
-        }
-    }
-
-    pub fn add(&self, value: usize) -> Result<(), &'static str> {
-        match self.value.write() {
-            Ok(mut write_guard) => {
-                *write_guard += value;
-                Ok(())
-            }
-            Err(_) => Err("Failed to acquire write lock"),
-        }
-    }
-
-    pub fn get_value(&self) -> Result<usize, &'static str> {
-        match self.value.read() {
-            Ok(read_guard) => Ok(*read_guard),
-            Err(_) => Err("Failed to acquire read lock"),
-        }
-    }
-}
 
 pub struct AtomicChannel<T> {
     sender: Mutex<mpsc::Sender<T>>,
